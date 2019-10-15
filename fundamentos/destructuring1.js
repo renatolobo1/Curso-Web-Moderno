9874c2ecf893df9b320b449137dd484829b110b4
renato <renatolobo_@hotmail.com>
2019-10-15 09:21:48 -0300

    Aula Destructuring 1

---
+++ fundamentos/destructuring1.js
@@ -0,0 +1,25 @@
+// Novo recurso do ES2015
+
+const pessoa = {
+    nome: 'Ana',
+    idade: 5,
+    endereco: {
+        logradouro: 'Rua ABC',
+        numero: 1000
+    }
+}
+
+const { nome, idade} = pessoa
+console.log(nome, idade)
+
+const { nome: n, idade: i} = pessoa
+console.log(n, i)
+
+const {sobrenome, sexo = true} = pessoa
+console.log(sobrenome, sexo)
+
+const {endereco: {logradouro, numero, cep} } = pessoa
+console.log(logradouro, numero, cep)
+
+const { conta: {ag, num}} = pessoa
+console.log(ag, num)
