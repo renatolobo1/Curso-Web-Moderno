a2a23f92875c2aea9043c9a6ce960fbd8b4cd052
Renato <renatolobo_@hotmail.com>
2019-10-22 16:10:15 -0300

    Oi

--- controle/switch.js
+++ controle/switch.js
@@ -3,6 +3,18 @@ const imprimirResultado = function (nota) {
         case 10:
         case 9:
             console.log('Quadro de Honra')
+            break // Por padrão tem que ter em cada um dos cases
+        case 8: case 7:
+            console.log('Aprovado')
+            break
+        case 6: case 5: case 4:
+            console.log('Recuperação')
+            break
+        case 3: case 2: case 1: case 0:
+            console.log('Reprovado')
+            break
+        default:
+            console.log('Nota Invalida')
     }
 } 
-imprimirResultado(7)
+imprimirResultado(4)
