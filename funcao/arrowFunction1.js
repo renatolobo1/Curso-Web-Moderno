a50381b945df45bdbc457e6c8331f290e6e7ac23
Renato <renatolobo_@hotmail.com>
2019-11-04 09:55:55 -0300

    Aula Funcao arrow 1

---
+++ funcao/arrowFunction1.js
@@ -0,0 +1,20 @@
+let dobro = function (a) {
+    return 2 * a
+}
+
+dobro = (a) => {
+    return 2 * a
+}
+
+dobro = a => 2 *a  // return esta implícito
+
+console.log(dobro(Math.PI))
+
+let ola = function(){
+    return 'Ola'
+}
+
+ola = () => 'Olá'
+ola = _ => 'Olá'
+
+console.log(ola())
