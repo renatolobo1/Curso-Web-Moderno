c12122eb5cd062dd23e22c50364e59d821700030
Renato <renatolobo_@hotmail.com>
2019-11-06 11:00:05 -0300

    Aula tipos de declaraçao

---
+++ funcao/tiposDeclaracao.js
@@ -0,0 +1,19 @@
+console.log(soma(3, 4))
+
+
+// fuction delaration
+function soma(x, y){
+    return x + y
+}
+
+// function expression
+
+const sub = function (x, y){
+    return x - y
+}
+
+// named function expression  POUCO USADA
+
+const mult = function mult(x , y){
+    return x * y
+}
