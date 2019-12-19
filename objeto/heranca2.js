e9a70e5c3d9fcb7859b53ddf92a1d4ad46ea295c
Renato <renatolobo_@hotmail.com>
2019-12-19 11:22:51 -0300

    Aual herança 2

---
+++ objeto/heranca2.js
@@ -0,0 +1,45 @@
+// Cadeia de protótipos (prototype chain)
+Object.prototype.attr0 = '0' // Não faça isso em casa
+const avo = { attr1: 'A'}
+const pai = { __proto__: avo, attr2: 'B', attr3: 'C'}
+const filho = { __proto__: pai, attr3: 'C'}
+console.log(filho.attr0, filho.attr1, filho.attr2, filho.attr3)
+
+const carro = {
+    velAtual: 0,
+    velMax: 300,
+    acelararMais(delta){
+        if (this.velAtual + delta <= this.velMax) {
+            this.velAtual += delta
+        } else {
+            this.velAtual = this.velMax
+        }
+    },
+    status(){
+        return `${this.velAtual}Km/h de ${this.velMax}Km/h`
+    }
+}
+
+const ferrari = {
+    modelo: 'F40',
+    velMax: 324 // Shodowing
+}
+
+const volvo = {
+    modelo: 'V40',
+    status(){
+        return `${this.modelo}: ${super.status()}`
+    }
+}
+
+Object.setPrototypeOf(ferrari, carro) // função que estabelece prototipo 
+Object.setPrototypeOf(volvo, carro)
+
+console.log(ferrari)
+console.log(volvo)
+
+volvo.acelararMais(100)
+console.log(volvo.status())
+
+ferrari.acelararMais(300)
+console.log(ferrari.status())
